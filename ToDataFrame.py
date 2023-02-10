4d6454405b9fc88ac8d4b28e061835dc3c65dce8
Andreas Burbach <aburbach@compeon.de>
2023-02-10 14:40:14 +0100

    update

--- ToDataFrame.py
+++ ToDataFrame.py
@@ -1,5 +1,26 @@
+import json
 import pandas as pd
 
-def stock_dataset_file_to_DataFrame(path: str) -> pd.DataFrame:
+def stock_dataset_file_to_DataFrame(path: str, stockName: str) -> pd.DataFrame:
+  """Reads a json file and returns a DataFrame with the data of the stockName
+
+  Args:
+      path (str): File path to the json file
+      stockName (str): Name of the stock to be read to the DataFrame
+
+  Raises:
+      ValueError: If the stockName is not found in the dataset
+
+  Returns:
+      pd.DataFrame: DataFrame with the data of the stockName
+  """
   
-  return result
+  
+  #read the json file
+  with open(path, 'r') as f:
+    data = json.load(f)
+    
+  if stockName not in data:
+    raise ValueError("Stock name not found in dataset")
+  
+  return pd.DataFrame(data[stockName], columns=["DateTime", "Value"])
